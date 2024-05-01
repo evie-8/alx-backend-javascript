e14845d2613ca59638f412eed57008fdb8ce0d07
evie-8 <neouma29@gmail.com>
2024-05-01 20:27:15 +0300

    corrections

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,22 +2,22 @@
 
 const assert = require('chai');
 const calculateNumber = require('./2-calcul_chai');
-const expect = assert.except;
+const expect = assert.expect;
 
 describe('testing the calculator', () => {
   it('should add Numbers correctly', () => {
-    except.equal(calculateNumber('SUM', 2, 4), 6);
+    expect.equal(calculateNumber('SUM', 2, 4), 6);
   });
   it('should add floating point numbers correctly', () => {
-    except.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    expect.equal(calculateNumber('SUM', 1.4, 4.5), 6);
   });
   it('should subtract numbers correctly', () => {
-    except.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    expect.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   });
   it('should Divide numbers correctly', () => {
-    except.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    expect.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
   });
   it('should raise an error when dividing with zero', () => {
-    except.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    expect.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
   });
 });
